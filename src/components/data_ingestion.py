cd6290124504a361803a70130ec4164fe41c9be7
Vipina7 <vipina1394@gmail.com>
2025-01-26 17:28:00 +0530

    prediction pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -43,9 +44,3 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e,sys)
         
-if __name__ == "__main__":
-    obj = DataIngestion()
-    train_data, test_data = obj.initiate_data_ingestion()
-
-    data_transformation_obj = DataTransformation()
-    data_transformation_obj.initiate_data_transformation(train_data, test_data)
